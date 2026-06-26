3ce4559164db1de615301295113bd0488ab2f7a4
mantu-morya-simform <mantu.morya@simformsolutions.com>
2026-06-26 19:46:14 +0530

    Remove unused and outdated versions of useLocalStorage and usePrev hooks, along with related App component files. Clean up history to maintain only relevant code.

--- Pattern/HOC/src/component/ComponentWrapper.tsx
+++
@@ -1,6 +0,0 @@
-import HocWrapper from "../HOC/HocWrapper";
-import Analysis from "./Analysis";
-import Posts from "./Posts";
-
-export const PostHoc = HocWrapper(Posts);
-export const AnalysisHoc = HocWrapper(Analysis);
